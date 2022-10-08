856af1d0921c9dc38daa80658ed363171292470f
sagoresarker <sagoresarker.cse@gmail.com>
2022-10-08 17:00:28 +0600

    Add redirection on solution page

--- website/base/urls.py
+++ website/base/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('features', views.features, name='features'),
+    path('pricing', views.pricing, name='pricing'),
+    path('solutions', views.solutions, name='solutions'),
+    path('solutions/<str:id>', views.single_solutions, name='single-solutions'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='home')
 ]
--- website/base/views.py
+++ website/base/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+def solutions(request):
+    context = {}
+    return render(request, 'base/solutions.html', context)
+
+
+def single_solutions(request, pk):
+    next = ""
+
+    if request.GET:
+        next = request.GET['next']
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        if next == "":
+            return HttpResponseRedirect('home')
+        else:
+            return HttpResponseRedirect(next)
+
+    context = {'id':pk}
+    return render(request, 'base/single-solution.html', context)
+
+def features(request):
+    context = {}
+    return render(request,'base/features.html', context)
+
+def pricing(request):
+    context = {}
+    return render(request,'base/pricing.html', context)
+
+def about(request):
+    context = {}
+    return render(request,'base/about.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request,'base/contact.html', context)
